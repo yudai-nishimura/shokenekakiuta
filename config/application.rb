0dd082cfc6144239c3cf3aafdaea310d0c6b75f9
yudai-nishimura <yudai.n0628@gmail.com>
2023-06-01 21:48:24 +0900

    Add: タイムゾーンを日本時間に設定 #6

--- config/application.rb
+++ config/application.rb
@@ -23,12 +23,9 @@ module Shokenekakiuta
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
+    # タイムゾーン設定
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
