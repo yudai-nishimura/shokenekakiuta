10d9058532e56d8fe520d4bd60da5acd41d1ad40
yudai-nishimura <yudai.n0628@gmail.com>
2023-06-01 22:13:35 +0900

    Add: デフォルトの言語を日本語に設定 #6

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,11 @@ module Shokenekakiuta
     # タイムゾーン設定
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+
+    # 言語設定
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
+    
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
