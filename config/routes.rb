6256ea7836403776cf266e1185a86424462b70b1
yudai-nishimura <yudai.n0628@gmail.com>
2023-06-05 19:21:21 +0900

    Add: ルーティングを設定 #18

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
 
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+
   get 'signup', to: 'users#new'
   resources :users, only: %i[create]
 end
