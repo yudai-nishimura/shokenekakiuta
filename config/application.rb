c445d2eef6742dcae3c3e0684ee639d76f0592ae
yudai-nishimura <yudai.n0628@gmail.com>
2023-06-07 18:39:59 +0900

    Improve: RuboCopに指摘されたコードを改善 #24

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module Shokenekakiuta
     # 言語設定
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
-    
+
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
