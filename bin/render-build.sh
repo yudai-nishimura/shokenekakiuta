055954abdf7b84c7d68584c52f297e723523c27f
yudai-nishimura <yudai.n0628@gmail.com>
2023-06-15 13:23:01 +0900

    Change: 変更を元に戻す #36

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,6 +6,4 @@ bundle install
 yarn install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rails webpacker:install
-bundle exec rails webpacker:compile
 bundle exec rake db:migrate
